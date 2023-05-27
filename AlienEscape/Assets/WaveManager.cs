f219f39ad46d8c25bcb62a7d95b3f8069b737321
xavzzzz <xavier.nicolle1@gmail.com>
2023-05-27 16:35:47 +0200

    +start of sin wave prefab

--- AlienEscape/Assets/WaveManager.cs
+++ AlienEscape/Assets/WaveManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public LineRenderer myLineRenderer;
+    public int points; 
+    public float amplitude = 1;
+    public float frequency = 1;
+    public float movementSpeed = 1;
+    public Vector2 Limit = new Vector2(0, 1);
+
+    public void Awake()
     {
-        
+        myLineRenderer = GetComponent<LineRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Draw()
     {
-        
+
+        float Start =  Limit.x;
+        float Tau  = 2 * Mathf.PI;
+        float Finish = Limit.y;
+        myLineRenderer.positionCount = points;
+
+        for (int currentPoint = 0; currentPoint < points; currentPoint++) {
+
+            float progress = (float)currentPoint / (points - 1); 
+            float x = Mathf.Lerp(Start, Finish, progress);
+            float y = amplitude * Mathf.Sin((Tau * frequency * x) + (Time.timeSinceLevelLoad* movementSpeed)); 
+            
+            myLineRenderer.SetPosition(currentPoint, new Vector3(x, y, 0));
+        }
+
+    }
+
+    public void FixedUpdate() {
+        Draw();
     }
 }
