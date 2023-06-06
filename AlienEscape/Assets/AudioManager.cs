ede0b6f43ad4119317438683f57a5c83730f304d
xavzzzz <xavier.nicolle1@gmail.com>
2023-06-06 10:21:07 +0200

    +hotfix

---
+++ AlienEscape/Assets/AudioManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource CurrentCassetteTrack;
+
+    public void PlayCasette() {
+        
+
+    }
+
+    public void GetCasetteAudio()
+    {
+
+
+    }
+
+
+
+}
