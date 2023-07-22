5b724b7ed4df9c2e4c734c388829bdf16e39e167
Valdoris <tiganoeric@gmail.com>
2023-07-22 15:40:32 +0200

    Runes push inté, + new sounds design

--- AlienEscape/Assets/Application/RuneSequencer.cs
+++ AlienEscape/Assets/Application/RuneSequencer.cs
@@ -11,23 +11,29 @@ public class RuneSequencer : MonoBehaviour
     private int counter;
     private int tries;
 
+    private FMOD.Studio.EventInstance RuneFail;
+    private FMOD.Studio.EventInstance RuneRight;
+
 
     public void Awake()
     {
 
         counter = 1;
         tries = 0;
+        RuneFail = FMODUnity.RuntimeManager.CreateInstance("event:/Artefact/Artefact_RuneFailed");
+        RuneRight = FMODUnity.RuntimeManager.CreateInstance("event:/Artefact/Artefact_Artefact_RuneWin");
     }
 
     public void TrySequence(Rune a)
     {
         Debug.Log("try");
 
-        if (tries >= MaxSequence) { Debug.Log("Loser let's reset"); }
+        if (tries >= MaxSequence) { Debug.Log("Loser let's reset"); RuneFail.start(); }
 
 
         if (a.OrderInSequence == counter || a.OrderInSequence == MaxSequence-counter+1)
         {
+            RuneRight.start();
 
             if (counter == MaxSequence) { Debug.Log("T TRO FORT SALE RACISTE"); return; } 
             Debug.Log("Can continue");
