231da14829662ededcdc064b16a62ac1b5ab42ab
xavzzzz <36257240+xavzzzz@users.noreply.github.com>
2023-07-25 13:51:15 +0200

    +laser decals good enough

--- AlienEscape/Assets/Application/RaygunManager.cs
+++ AlienEscape/Assets/Application/RaygunManager.cs
@@ -1,12 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class RaygunManager : MonoBehaviour
 {
     private bool shooting;
+    private bool canShootDecals = true;
 
     public bool Constructed;
     public Collider Laser;
@@ -16,73 +14,65 @@ public class RaygunManager : MonoBehaviour
     public float fireRate;
     public float laserDuration;
 
-    
-
     LineRenderer laserLine;
     float fireTimer;
+    RaycastHit previousHit;
 
     void Awake()
     {
         laserLine = this.GetComponentInChildren<LineRenderer>();
         laserOrigin = laserLine.GetComponent<Transform>();
-        
     }
-  
 
     public void Shoot()
     {
-
         shooting = true;
-        
     }
 
     public void StopShoot()
     {
-        
         shooting = false;
         laserLine.enabled = false;
+        canShootDecals = false;
     }
 
-    public void FixedUpdate()
+    public void Update()
     {
-        
-
         fireTimer += Time.deltaTime;
         if (shooting && fireTimer >= fireRate)
         {
-            
-
-            Debug.Log("Shhot");
             fireTimer = 0;
             laserLine.SetPosition(0, laserOrigin.position);
             Vector3 rayOrigin = laserLine.gameObject.transform.position;
             RaycastHit hit;
+
             if (Physics.Raycast(rayOrigin, laserLine.gameObject.transform.forward, out hit, gunRange))
             {
+                if(canShootDecals)StartCoroutine(DoDecals(previousHit, hit));
+                previousHit = hit;
                 var hitObj = hit.collider.gameObject;
-                
-
 
                 laserLine.SetPosition(1, hit.point);
                 if (hitObj.CompareTag("LightLink"))
                 {
                     var lightRune = hitObj.GetComponent<Rune>();
 
-                    if(!lightRune.locked)hitObj.GetComponentInParent<RuneSequencer>().TrySequence(lightRune);
+                    if (!lightRune.locked) hitObj.GetComponentInParent<RuneSequencer>().TrySequence(lightRune);
                     lightRune.locked = true;
 
                     hitObj.GetComponent<Renderer>().material.color = Color.green;
                 }
-                
-                    this.GetComponent<DecalPainter>().PaintDecal(hit.point, hit.normal, hit.collider);
-                
+
+                canShootDecals = true;
             }
             else
             {
                 laserLine.SetPosition(1, rayOrigin + (laserLine.gameObject.transform.forward * gunRange));
             }
             StartCoroutine(ShootLaser());
+            
         }
+        
     }
 
     IEnumerator ShootLaser()
@@ -90,8 +80,24 @@ public class RaygunManager : MonoBehaviour
         laserLine.enabled = true;
         yield return new WaitForSeconds(laserDuration);
     }
+
+    IEnumerator DoDecals(RaycastHit previousHit, RaycastHit hit)
+    {
+        float duration = 0.05f;
+        int numSteps = 10;
+        float stepDelay = duration / numSteps;
+
+        for (int i = 0; i < numSteps; i++)
+        {
+            float lerpAmount = (float)i / (numSteps - 1);
+            Vector3 decalPosition = Vector3.Lerp(previousHit.point, hit.point, lerpAmount);
+
+            this.GetComponent<DecalPainter>().PaintDecal(decalPosition, hit.normal, hit.collider);
+
+            yield return new WaitForSeconds(stepDelay);
+        }
+
+        
+
+    }
 }
-
-
-
-
