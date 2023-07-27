1e4aba5865c204c7f891492e762aee9efe26a05e
Valdoris <tiganoeric@gmail.com>
2023-07-27 20:35:21 +0200

    modif scene final scene push for camoll

--- AlienEscape/Assets/Application/RuneManager.cs
+++ AlienEscape/Assets/Application/RuneManager.cs
@@ -7,21 +7,31 @@ public class RuneManager : MonoBehaviour
     public Material RuneLook;
     public Material DefaultLook;
 
+    private FMOD.Studio.EventInstance Glow;
+
     public bool Unlocked;
 
     private void Awake()
     {
         Unlocked = false;
+        Glow = FMODUnity.RuntimeManager.CreateInstance("event:/Artefact/Artefact_RuneGlowing");
+
     }
 
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Rune" && Unlocked) other.GetComponent<Renderer>().material = RuneLook;
+        if (other.tag == "Rune" && Unlocked)
+        {
+            Glow.start();
+            other.GetComponent<Renderer>().material = RuneLook;
+            Glow.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(other.gameObject.transform.position));
+        }
     }
 
     void OnTriggerExit(Collider other)
         {
+        Glow.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         if (other.tag == "Rune" && Unlocked) other.GetComponent<Renderer>().material = DefaultLook;
         }
     
