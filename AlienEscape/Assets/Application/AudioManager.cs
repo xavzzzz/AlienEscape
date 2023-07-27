5e5e6ff54bfd7de4a4a60461c43a1994d636b84b
Valdoris <tiganoeric@gmail.com>
2023-07-27 18:45:05 +0200

    Push change cassettes

--- AlienEscape/Assets/Application/AudioManager.cs
+++ AlienEscape/Assets/Application/AudioManager.cs
@@ -6,6 +6,26 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class AudioManager : MonoBehaviour
 {
     public int CurrentCassetteID = 0;
+    private FMOD.Studio.EventInstance Cassette_1;
+    private FMOD.Studio.EventInstance Cassette_2;
+    private FMOD.Studio.EventInstance Cassette_3;
+
+    private bool Cas1Paused;
+    private bool Cas2Paused;
+    private bool Cas3Paused;
+
+    private bool FromRewind;
+    private bool FromForward;
+
+
+    void Start()
+    {
+        Cassette_1 = FMODUnity.RuntimeManager.CreateInstance("event:/Cassetes/Cassette 1");
+        Cassette_2 = FMODUnity.RuntimeManager.CreateInstance("event:/Cassetes/Cassette 2");
+        Cassette_3 = FMODUnity.RuntimeManager.CreateInstance("event:/Cassetes/Cassette 3");
+
+    }
+
 
     public void OnSelectEntered(SelectEnterEventArgs args)
     {
@@ -15,7 +35,29 @@ public class AudioManager : MonoBehaviour
 
     public void EjectCasette()
     {
-        CurrentCassetteID = 0;
+
+        if (CurrentCassetteID != 0)
+        {
+
+            if (CurrentCassetteID == 1)
+            {
+                Cassette_3.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+                CurrentCassetteID = 0;
+            }
+
+            if (CurrentCassetteID == 2)
+            {
+                Cassette_3.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+                CurrentCassetteID = 0;
+            }
+
+            if (CurrentCassetteID == 3)
+            {
+                Cassette_3.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+                CurrentCassetteID = 0;
+            }
+        }
+        
     }
 
 
@@ -23,6 +65,9 @@ public class AudioManager : MonoBehaviour
     public void UpdateID(int x)
     {
         CurrentCassetteID = x;
+        Cassette_1.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        Cassette_2.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        Cassette_3.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
     }
 
     public void PlayCasette()
@@ -32,17 +77,62 @@ public class AudioManager : MonoBehaviour
 
             if (CurrentCassetteID == 1)
             {
-                //Play FMOD event 1
+                if (FromRewind == true)
+                {
+                    Cassette_1.getTimelinePosition(out int TimePosition);
+                    Cassette_1.setTimelinePosition((63097) - TimePosition);
+                    FromRewind = false;
+                    FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Cassette1_Rewind", 0);
+
+                }
+                else if( FromForward == true)
+                {
+                    FromForward = false;
+                    FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Cassette1_FastForward", 0);
+                }
+
+                else if (Cas1Paused)
+                {
+
+                    //Play FMOD event 1
+                    Cassette_1.setPaused(false);
+                    Cas1Paused = false;
+                }
+                else
+                {
+                    
+                    //Play FMOD event 1
+                    Cassette_1.start();
+                }
+                
             }
 
             if (CurrentCassetteID == 2)
             {
-                //Play FMOD event 2
+                if (Cas2Paused)
+                {
+                    //Play FMOD event 1
+                    Cassette_2.start();
+                }
+                else
+                {
+                    Cassette_2.setPaused(false);
+                    Cas2Paused = false;
+                }
             }
 
             if (CurrentCassetteID == 3)
             {
-                //Play FMOD event 3
+                if (Cas3Paused)
+                {
+                    //Play FMOD event 1
+                    Cassette_3.start();
+                }
+                else
+                {
+                    Cassette_3.setPaused(false);
+                    Cas1Paused = false;
+                }
             }
         }
     }
@@ -55,17 +145,81 @@ public class AudioManager : MonoBehaviour
             if (CurrentCassetteID == 1)
             {
                 //PAUSE FMOD event 1
+                Cassette_1.setPaused(true);
+                Cas1Paused = true;
             }
 
             if (CurrentCassetteID == 2)
             {
                 //Play FMOD event 2
+                Cassette_2.setPaused(true);
+                Cas2Paused = true;
             }
 
             if (CurrentCassetteID == 3)
             {
                 //Play FMOD event 3
+                Cassette_3.setPaused(true);
+                Cas3Paused = true;
             }
         }
     }
+    public void RewindCasette()
+    {
+        if (CurrentCassetteID != 0)
+        {
+
+            if (CurrentCassetteID == 1)
+            {
+                //PAUSE FMOD event 1
+                FromRewind = true;
+                FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Cassette1_Rewind", 1);
+
+            }
+
+            if (CurrentCassetteID == 2)
+            {
+                //Play FMOD event 2
+                FromRewind = true;
+                
+            }
+
+            if (CurrentCassetteID == 3)
+            {
+                //Play FMOD event 3
+                FromRewind = true;
+                
+            }
+        }
+    }
+
+    public void ForwardCasette()
+    {
+        if (CurrentCassetteID != 0)
+        {
+
+            if (CurrentCassetteID == 1)
+            {
+                //PAUSE FMOD event 1
+                FromForward = true;
+                FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Cassette1_FastForward", 1);
+
+            }
+
+            if (CurrentCassetteID == 2)
+            {
+                //Play FMOD event 2
+                FromForward = true;
+                FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Cassette1_FastForward", 1);
+            }
+
+            if (CurrentCassetteID == 3)
+            {
+                //Play FMOD event 3
+                FromForward = true;
+                FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Cassette1_FastForward", 1);
+            }
+        }
+    }
+
 }
