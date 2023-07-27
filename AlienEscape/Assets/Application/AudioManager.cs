29ef755cdaca0b285563243651bd377b66c1b02f
xavzzzz <36257240+xavzzzz@users.noreply.github.com>
2023-07-27 16:33:28 +0200

    +audioplayer fix

--- AlienEscape/Assets/Application/AudioManager.cs
+++ AlienEscape/Assets/Application/AudioManager.cs
@@ -13,8 +13,59 @@ public class AudioManager : MonoBehaviour
         UpdateID(i);
     }
 
+    public void EjectCasette()
+    {
+        CurrentCassetteID = 0;
+    }
+
+
+
     public void UpdateID(int x)
     {
         CurrentCassetteID = x;
+    }
+
+    public void PlayCasette()
+    {
+        if (CurrentCassetteID != 0)
+        {
+
+            if (CurrentCassetteID == 1)
+            {
+                //Play FMOD event 1
+            }
+
+            if (CurrentCassetteID == 2)
+            {
+                //Play FMOD event 2
+            }
+
+            if (CurrentCassetteID == 3)
+            {
+                //Play FMOD event 3
+            }
+        }
+    }
+
+    public void PauseCasette()
+    {
+        if (CurrentCassetteID != 0)
+        {
+
+            if (CurrentCassetteID == 1)
+            {
+                //PAUSE FMOD event 1
+            }
+
+            if (CurrentCassetteID == 2)
+            {
+                //Play FMOD event 2
+            }
+
+            if (CurrentCassetteID == 3)
+            {
+                //Play FMOD event 3
+            }
+        }
     }
 }
